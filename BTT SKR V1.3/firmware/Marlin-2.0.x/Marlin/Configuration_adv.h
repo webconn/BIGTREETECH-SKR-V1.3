695f432a341bbe5d3fd9b7556674f5067642821a
Nikita Maslov <wkernelteam@gmail.com>
2021-08-21 19:05:34 +0300

    disable stealthchop and reduce microstepping on z axis

--- BTT SKR V1.3/firmware/Marlin-2.0.x/Marlin/Configuration_adv.h
+++ BTT SKR V1.3/firmware/Marlin-2.0.x/Marlin/Configuration_adv.h
@@ -2263,7 +2263,7 @@
   #if AXIS_IS_TMC(Z)
     #define Z_CURRENT       800
     #define Z_CURRENT_HOME  Z_CURRENT
-    #define Z_MICROSTEPS     256
+    #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
     #define Z_CHAIN_POS      -1
   #endif
@@ -2421,8 +2421,8 @@
    * When disabled, Marlin will use spreadCycle stepping mode.
    */
   #define STEALTHCHOP_XY
-  #define STEALTHCHOP_Z
-  #define STEALTHCHOP_E
+  //#define STEALTHCHOP_Z
+  //#define STEALTHCHOP_E
 
   /**
    * Optimize spreadCycle chopper parameters by using predefined parameter sets
